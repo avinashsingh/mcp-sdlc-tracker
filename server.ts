6907e973cd2192ed0b94b2b67731f24d11a36684
Avinash Singh <avinash@dgist.in>
2025-11-24 11:45:57 +0530

    Fix all ambiguous column references in SQL queries
    - Fix list_epics: e.status = ? (was ambiguous status = ?)
    - Fix list_tasks: t.status = ? (was potentially ambiguous status = ?)
    - Prevent 'ambiguous column name' SQL errors in JOIN queries
    - Ensure all column references in multi-table queries are qualified
    - All list tools now have unambiguous column references

--- server.ts
+++ server.ts
@@ -1876,7 +1876,7 @@ server.registerTool(
       }
 
       if (status) {
-        conditions.push('e.status = ?');
+        conditions.push('t.status = ?');
         params.push(status);
       }
 
